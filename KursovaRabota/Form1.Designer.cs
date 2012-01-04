cf34d19f9c97694132b3097e07ebd8f47bf98a4b
Venelin Stoykov <vkstoykov@gmail.com>
2012-01-04 23:36:15 +0200

    Create First Form With Simple Fields

--- KursovaRabota/Form1.Designer.cs
+++ KursovaRabota/Form1.Designer.cs
@@ -28,12 +28,295 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxSurName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.groupBoxSex = new System.Windows.Forms.GroupBox();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.groupBoxEducation = new System.Windows.Forms.GroupBox();
+            this.radioButtonEducation5 = new System.Windows.Forms.RadioButton();
+            this.radioButtonEducation4 = new System.Windows.Forms.RadioButton();
+            this.radioButtonEducation3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonEducation2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonEducation1 = new System.Windows.Forms.RadioButton();
+            this.groupBoxLanguages = new System.Windows.Forms.GroupBox();
+            this.checkBoxLangRu = new System.Windows.Forms.CheckBox();
+            this.checkBoxLangDe = new System.Windows.Forms.CheckBox();
+            this.checkBoxLangEn = new System.Windows.Forms.CheckBox();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelSurName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.checkBoxLangFr = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBoxSex.SuspendLayout();
+            this.groupBoxEducation.SuspendLayout();
+            this.groupBoxLanguages.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(95, 16);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxFirstName.TabIndex = 0;
+            // 
+            // textBoxSurName
+            // 
+            this.textBoxSurName.Location = new System.Drawing.Point(95, 42);
+            this.textBoxSurName.Name = "textBoxSurName";
+            this.textBoxSurName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxSurName.TabIndex = 1;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(95, 68);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxLastName.TabIndex = 2;
+            // 
+            // groupBoxSex
+            // 
+            this.groupBoxSex.Controls.Add(this.radioButtonFemale);
+            this.groupBoxSex.Controls.Add(this.radioButtonMale);
+            this.groupBoxSex.Location = new System.Drawing.Point(95, 95);
+            this.groupBoxSex.Name = "groupBoxSex";
+            this.groupBoxSex.Size = new System.Drawing.Size(200, 79);
+            this.groupBoxSex.TabIndex = 3;
+            this.groupBoxSex.TabStop = false;
+            this.groupBoxSex.Text = "Пол";
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(7, 44);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonFemale.TabIndex = 1;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Жена";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(7, 20);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(49, 17);
+            this.radioButtonMale.TabIndex = 0;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Мъж";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxEducation
+            // 
+            this.groupBoxEducation.Controls.Add(this.radioButtonEducation5);
+            this.groupBoxEducation.Controls.Add(this.radioButtonEducation4);
+            this.groupBoxEducation.Controls.Add(this.radioButtonEducation3);
+            this.groupBoxEducation.Controls.Add(this.radioButtonEducation2);
+            this.groupBoxEducation.Controls.Add(this.radioButtonEducation1);
+            this.groupBoxEducation.Location = new System.Drawing.Point(95, 183);
+            this.groupBoxEducation.Name = "groupBoxEducation";
+            this.groupBoxEducation.Size = new System.Drawing.Size(199, 144);
+            this.groupBoxEducation.TabIndex = 4;
+            this.groupBoxEducation.TabStop = false;
+            this.groupBoxEducation.Text = "Образование";
+            // 
+            // radioButtonEducation5
+            // 
+            this.radioButtonEducation5.AutoSize = true;
+            this.radioButtonEducation5.Location = new System.Drawing.Point(7, 116);
+            this.radioButtonEducation5.Name = "radioButtonEducation5";
+            this.radioButtonEducation5.Size = new System.Drawing.Size(111, 17);
+            this.radioButtonEducation5.TabIndex = 4;
+            this.radioButtonEducation5.TabStop = true;
+            this.radioButtonEducation5.Text = "Висше Магистър";
+            this.radioButtonEducation5.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEducation4
+            // 
+            this.radioButtonEducation4.AutoSize = true;
+            this.radioButtonEducation4.Location = new System.Drawing.Point(7, 92);
+            this.radioButtonEducation4.Name = "radioButtonEducation4";
+            this.radioButtonEducation4.Size = new System.Drawing.Size(117, 17);
+            this.radioButtonEducation4.TabIndex = 3;
+            this.radioButtonEducation4.TabStop = true;
+            this.radioButtonEducation4.Text = "Висше Бакалавър";
+            this.radioButtonEducation4.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEducation3
+            // 
+            this.radioButtonEducation3.AutoSize = true;
+            this.radioButtonEducation3.Location = new System.Drawing.Point(7, 68);
+            this.radioButtonEducation3.Name = "radioButtonEducation3";
+            this.radioButtonEducation3.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonEducation3.TabIndex = 2;
+            this.radioButtonEducation3.TabStop = true;
+            this.radioButtonEducation3.Text = "Средно";
+            this.radioButtonEducation3.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEducation2
+            // 
+            this.radioButtonEducation2.AutoSize = true;
+            this.radioButtonEducation2.Location = new System.Drawing.Point(7, 44);
+            this.radioButtonEducation2.Name = "radioButtonEducation2";
+            this.radioButtonEducation2.Size = new System.Drawing.Size(69, 17);
+            this.radioButtonEducation2.TabIndex = 1;
+            this.radioButtonEducation2.TabStop = true;
+            this.radioButtonEducation2.Text = "Основно";
+            this.radioButtonEducation2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEducation1
+            // 
+            this.radioButtonEducation1.AutoSize = true;
+            this.radioButtonEducation1.Location = new System.Drawing.Point(7, 20);
+            this.radioButtonEducation1.Name = "radioButtonEducation1";
+            this.radioButtonEducation1.Size = new System.Drawing.Size(68, 17);
+            this.radioButtonEducation1.TabIndex = 0;
+            this.radioButtonEducation1.TabStop = true;
+            this.radioButtonEducation1.Text = "Начално";
+            this.radioButtonEducation1.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxLanguages
+            // 
+            this.groupBoxLanguages.Controls.Add(this.checkBoxLangFr);
+            this.groupBoxLanguages.Controls.Add(this.checkBoxLangRu);
+            this.groupBoxLanguages.Controls.Add(this.checkBoxLangDe);
+            this.groupBoxLanguages.Controls.Add(this.checkBoxLangEn);
+            this.groupBoxLanguages.Location = new System.Drawing.Point(301, 12);
+            this.groupBoxLanguages.Name = "groupBoxLanguages";
+            this.groupBoxLanguages.Size = new System.Drawing.Size(223, 162);
+            this.groupBoxLanguages.TabIndex = 5;
+            this.groupBoxLanguages.TabStop = false;
+            this.groupBoxLanguages.Text = "Чужди Езици";
+            // 
+            // checkBoxLangRu
+            // 
+            this.checkBoxLangRu.AutoSize = true;
+            this.checkBoxLangRu.Location = new System.Drawing.Point(7, 68);
+            this.checkBoxLangRu.Name = "checkBoxLangRu";
+            this.checkBoxLangRu.Size = new System.Drawing.Size(56, 17);
+            this.checkBoxLangRu.TabIndex = 2;
+            this.checkBoxLangRu.Text = "Руски";
+            this.checkBoxLangRu.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLangDe
+            // 
+            this.checkBoxLangDe.AutoSize = true;
+            this.checkBoxLangDe.Location = new System.Drawing.Point(7, 44);
+            this.checkBoxLangDe.Name = "checkBoxLangDe";
+            this.checkBoxLangDe.Size = new System.Drawing.Size(66, 17);
+            this.checkBoxLangDe.TabIndex = 1;
+            this.checkBoxLangDe.Text = "Немски";
+            this.checkBoxLangDe.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLangEn
+            // 
+            this.checkBoxLangEn.AutoSize = true;
+            this.checkBoxLangEn.Location = new System.Drawing.Point(7, 20);
+            this.checkBoxLangEn.Name = "checkBoxLangEn";
+            this.checkBoxLangEn.Size = new System.Drawing.Size(80, 17);
+            this.checkBoxLangEn.TabIndex = 0;
+            this.checkBoxLangEn.Text = "Английски";
+            this.checkBoxLangEn.UseVisualStyleBackColor = true;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(60, 19);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(29, 13);
+            this.labelFirstName.TabIndex = 6;
+            this.labelFirstName.Text = "Име";
+            // 
+            // labelSurName
+            // 
+            this.labelSurName.AutoSize = true;
+            this.labelSurName.Location = new System.Drawing.Point(36, 45);
+            this.labelSurName.Name = "labelSurName";
+            this.labelSurName.Size = new System.Drawing.Size(53, 13);
+            this.labelSurName.TabIndex = 7;
+            this.labelSurName.Text = "Презиме";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(33, 71);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(56, 13);
+            this.labelLastName.TabIndex = 8;
+            this.labelLastName.Text = "Фамилия";
+            // 
+            // checkBoxLangFr
+            // 
+            this.checkBoxLangFr.AutoSize = true;
+            this.checkBoxLangFr.Location = new System.Drawing.Point(7, 92);
+            this.checkBoxLangFr.Name = "checkBoxLangFr";
+            this.checkBoxLangFr.Size = new System.Drawing.Size(73, 17);
+            this.checkBoxLangFr.TabIndex = 3;
+            this.checkBoxLangFr.Text = "Френски";
+            this.checkBoxLangFr.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(470, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(557, 329);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelSurName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.groupBoxLanguages);
+            this.Controls.Add(this.groupBoxEducation);
+            this.Controls.Add(this.groupBoxSex);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxSurName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Name = "Form1";
+            this.Text = "Фирмени Работници";
+            this.groupBoxSex.ResumeLayout(false);
+            this.groupBoxSex.PerformLayout();
+            this.groupBoxEducation.ResumeLayout(false);
+            this.groupBoxEducation.PerformLayout();
+            this.groupBoxLanguages.ResumeLayout(false);
+            this.groupBoxLanguages.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxSurName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.GroupBox groupBoxSex;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.GroupBox groupBoxEducation;
+        private System.Windows.Forms.RadioButton radioButtonEducation5;
+        private System.Windows.Forms.RadioButton radioButtonEducation4;
+        private System.Windows.Forms.RadioButton radioButtonEducation3;
+        private System.Windows.Forms.RadioButton radioButtonEducation2;
+        private System.Windows.Forms.RadioButton radioButtonEducation1;
+        private System.Windows.Forms.GroupBox groupBoxLanguages;
+        private System.Windows.Forms.CheckBox checkBoxLangRu;
+        private System.Windows.Forms.CheckBox checkBoxLangDe;
+        private System.Windows.Forms.CheckBox checkBoxLangEn;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelSurName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.CheckBox checkBoxLangFr;
+        private System.Windows.Forms.Button button1;
     }
 }
 
